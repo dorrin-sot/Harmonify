54a22e73530b3790e8d4ed6075308460a03e9736
dorrin-sot <dorrinsotoudeh123@gmail.com>
2025-07-19 13:34:41 +0330

    Refactor RepeatIconButton to use property delegation for repeat mode

--- app/src/main/java/com/dorrin/harmonify/view/RepeatIconButton.kt
+++ app/src/main/java/com/dorrin/harmonify/view/RepeatIconButton.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.icons.filled.RepeatOneOn
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.media3.common.Player
@@ -18,20 +19,19 @@ import com.dorrin.harmonify.viewmodel.PlayerViewModel
 fun RepeatIconButton(
   playerViewModel: PlayerViewModel = hiltViewModel(LocalActivity.current as ComponentActivity)
 ) {
-  val repeatMode = playerViewModel.repeatMode.observeAsState()
+  val repeatMode by playerViewModel.repeatMode.observeAsState()
 
-  val value = repeatMode.value
-  if (value != null) {
-    IconButton(onClick = { playerViewModel.setRepeatMode((value + 1) % 3) }) {
+  repeatMode?.let {
+    IconButton(onClick = { playerViewModel.setRepeatMode((it + 1) % 3) }) {
       Icon(
-        when (value) {
+        when (it) {
           Player.REPEAT_MODE_ONE -> Icons.Default.RepeatOneOn
           Player.REPEAT_MODE_ALL -> Icons.Default.RepeatOn
           Player.REPEAT_MODE_OFF -> Icons.Default.Repeat
           else -> Icons.Default.Repeat
         },
         contentDescription =
-          when (value) {
+          when (it) {
             Player.REPEAT_MODE_ONE -> "Repeat One"
             Player.REPEAT_MODE_ALL -> "Repeat All"
             Player.REPEAT_MODE_OFF -> "Repeat Off"
