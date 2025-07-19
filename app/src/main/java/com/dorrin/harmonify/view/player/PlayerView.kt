a1e1e2348a5bb32bfe346d2c790fa9d9a115781b
dorrin-sot <dorrinsotoudeh123@gmail.com>
2025-07-19 13:56:56 +0330

    Refactor RotatingVinylView to use property delegation for track state

--- app/src/main/java/com/dorrin/harmonify/view/player/PlayerView.kt
+++ app/src/main/java/com/dorrin/harmonify/view/player/PlayerView.kt
@@ -20,11 +20,8 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
