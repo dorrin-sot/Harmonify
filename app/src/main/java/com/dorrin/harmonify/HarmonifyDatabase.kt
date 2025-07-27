25c0ff199f638f12c7a2e3959f15430c9b1a2f53
dorrin-sot <dorrinsotoudeh123@gmail.com>
2025-07-27 14:51:58 +0330

    Update HarmonifyDatabase to include Like entities and version increment

--- app/src/main/java/com/dorrin/harmonify/HarmonifyDatabase.kt
+++ app/src/main/java/com/dorrin/harmonify/HarmonifyDatabase.kt
@@ -2,14 +2,17 @@ package com.dorrin.harmonify
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.dorrin.harmonify.conversion.DBConverters
 import com.dorrin.harmonify.dao.AlbumDao
 import com.dorrin.harmonify.dao.ArtistDao
 import com.dorrin.harmonify.dao.TrackDao
-import com.dorrin.harmonify.model.Album
-import com.dorrin.harmonify.model.Artist
-import com.dorrin.harmonify.model.Track
+import com.dorrin.harmonify.entities.AlbumLike
+import com.dorrin.harmonify.entities.ArtistLike
+import com.dorrin.harmonify.entities.TrackLike
 
-@Database(entities = [Album::class, Artist::class, Track::class], version = 1)
+@Database(entities = [AlbumLike::class, ArtistLike::class, TrackLike::class], version = 3)
+@TypeConverters(DBConverters::class)
 abstract class HarmonifyDatabase : RoomDatabase() {
   abstract fun albumDao(): AlbumDao
   abstract fun artistDao(): ArtistDao
